4c44a9262d46e990383461308bafab33d10f1f96
gleilsonbarbosa <eliteacaimkt@gmail.com>
2025-08-11 21:22:28 -0300

    Atualizar raio da loja de teste para 40m

--- src/types.ts
+++ src/types.ts
@@ -107,6 +107,6 @@ export const TEST_STORE: StoreLocation = {
   name: 'Loja Teste',
   latitude: -3.859704392616844,
   longitude: -38.63435852000634,
-  radius: 1000, // 1km radius for testing
+  radius: 40, // 40 meters radius
   address: 'Endereço de Teste'
 };
